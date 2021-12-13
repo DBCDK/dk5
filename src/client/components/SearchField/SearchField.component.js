cb00afcccfe9f928778a4407d51bff6bc10e1805
Finn Stausgaard <fvs@dbc.dk>
2021-12-13 08:57:10 +0100

    Drift info

--- src/client/components/SearchField/SearchField.component.js
+++ src/client/components/SearchField/SearchField.component.js
@@ -239,7 +239,10 @@ export class SearchFieldComponent extends Component {
         className="search-field--container"
         onSubmit={this.queryWasSubmitted}
       >
-        <div>Mandag d. 13/12: Servicen er ude af drift. Vi forventer at problemerne er løst i løbet af dagen</div>
+        <h2>Mandag d. 13/12: Servicen er ude af drift. Vi forventer at problemerne er løst i løbet af dagen</h2>
+        <br />
+        <br />
+        <br />
         <div className="search-field--title">
           <label htmlFor="search"><h2>Find hylden med bøger om ...</h2></label>
         </div>
